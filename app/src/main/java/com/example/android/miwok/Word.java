2e3f5427ada0c24c858e248bf7e4b1bd6726aed0
rrangel <THI$i$@GHpw1!>
2018-05-27 10:45:39 -0500

    Created a single xml file for all translations call 'word_list' and deleted no longer used xml files.

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -1,11 +1,30 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.android.miwok;
 
+/**
+ * {@link Word} represents a vocabulary word that the user wants to learn.
+ * It contains a default translation and a Miwok translation for that word.
+ */
 public class Word {
 
-    // Default translation for the word
+    /** Default translation for the word */
     private String mDefaultTranslation;
 
-    //Miwork translation for the word
+    /** Miwok translation for the word */
     private String mMiwokTranslation;
 
     /**
@@ -14,7 +33,6 @@ public class Word {
      * @param defaultTranslation is the word in a language that the user is already familiar with
      *                           (such as English)
      * @param miwokTranslation is the word in the Miwok language
-     *
      */
     public Word(String defaultTranslation, String miwokTranslation) {
         mDefaultTranslation = defaultTranslation;
@@ -29,9 +47,10 @@ public class Word {
     }
 
     /**
-     * Get the Miwork translation of the word.
+     * Get the Miwok translation of the word.
      */
-    public String getMiworkTranslation() {
+    public String getMiwokTranslation() {
         return mMiwokTranslation;
     }
+
 }
